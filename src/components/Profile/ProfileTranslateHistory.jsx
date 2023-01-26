2bf604a1eab32ac756d2b8ed7fde5a807d3ad541
mosh98 <mosleh.edu@gmail.com>
2023-01-26 15:34:00 +0100

    delete works but append new item doesn't

--- src/components/Profile/ProfileTranslateHistory.jsx
+++ src/components/Profile/ProfileTranslateHistory.jsx
@@ -1,4 +1,5 @@
 import {useState,useEffect} from 'react';
+import {createHeaders} from "./index";
 const apiURL = process.env.REACT_APP_API_URL
 
 const ProfileTranslateHistory = () => {
@@ -14,10 +15,14 @@ const ProfileTranslateHistory = () => {
             const historyFromServer = await fetchHistory();
 
             //setHistory(historyFromServer.slice(0,10));
+            //historyFromServer is already reversed from the fetchHistory function
             setHistory(historyFromServer.slice(0,10));
         }
         getHistory();
     },[]);
+
+    //make a fetch history function
+
 
     const fetchHistory = async () => {
         try {
@@ -36,9 +41,36 @@ const ProfileTranslateHistory = () => {
         }
 
     }
-    //make a clear history onclick function that removes the rendered history but does not clear database
+
+    const clearHistoryFromAPI = async () => {
+        let userId = JSON.parse(localStorage.getItem('translator')).id
+
+        //make a patch request to the API on translations with EMPTY list
+        try {
+            const response = await fetch(`${apiURL}/${userId}`, {
+                method: 'PATCH',
+                headers: createHeaders(),
+                body: JSON.stringify({translations: []})
+            })
+            if (!response.ok) {
+                throw new Error("Could not fetch the data for that resource");
+            }
+            const data = await response.json();
+            return data;
+        } catch (e) {
+            console.log(e);
+            return e
+
+        }
+    }
+
+    //make a clear history onclick function that removes the rendered history from the page
     const clearHistory = () => {
         setHistory([]);
+        //also clear history from the API
+        clearHistoryFromAPI();
+
+
 
     }
 
